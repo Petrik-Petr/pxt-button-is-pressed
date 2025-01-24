40a46db19ca0a4ab0954f4d3afab93b470c5f5d7
Pepé <petr.petrik.024@pslib.cz>
2025-01-24 11:01:00 +0100

    main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,17 @@
-input.onButtonPressed(Button.A, function () {
-    music.setVolume(180)
-    music.ringTone(Note.C)
-    pause(50)
-    music.stopAllSounds()
+let A = 1
+basic.forever(function(){
+    if (A === 1)
+        if (input.buttonIsPressed(Button.A) === true){
+            music.setVolume(255)
+            music.ringTone(Note.C)
+            basic.pause(200)
+            music.setVolume(0)
+            A = 0
+        }
+    
+    if (A === 0){
+        if (input.buttonIsPressed(Button.A) === false){
+            A = 1
+        }
+    }
 })
